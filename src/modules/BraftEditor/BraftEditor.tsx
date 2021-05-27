22538d623a2c50b48ed74599e0252f1177f5ee6c
Linmingdao <mr.lmd@outlook.com>
2021-05-27 19:18:41 +0800

    update：删除无用组件

--- src/modules/BraftEditor/BraftEditor.tsx
+++ src/modules/BraftEditor/BraftEditor.tsx
@@ -6,9 +6,6 @@ import BraftEditor from "braft-editor";
 import React, { useState } from "react";
 import { TitleLayout, FilterBox } from "tetris-ui";
 import "./braftEditorExtension.js";
-import "react-area-linkage/dist/index.css";
-import { pcaa } from "area-data"; // v3 or higher
-import { AreaSelect, AreaCascader } from "react-area-linkage";
 
 const BraftEditorDemo: React.FC = () => {
   const [isReadOnly, setIsReadOnly] = useState(false);
@@ -19,23 +16,8 @@ const BraftEditorDemo: React.FC = () => {
     setEditorState(editorState);
   }
 
-  function handleAreaChange(val: any) {
-    console.log(val);
-  }
-
   return (
     <TitleLayout hasBack title="Braft">
-      <AreaCascader
-        level={1}
-        defaultArea={["110000", "110100", "110101"]}
-        onChange={handleAreaChange}
-        data={pcaa}
-      />
-      <AreaSelect
-        level={2}
-        data={pcaa}
-        defaultArea={["110000", "110100", "110101"]}
-      />
       <FilterBox>
         <Button type="text" onClick={() => setIsReadOnly(!isReadOnly)}>
           当前处于：{isReadOnly ? "只读模式" : "可编辑模式"}
